5434592a95b481bf0c936149be1073cd679b0d7a
mityu <mityu.mail@gmail.com>
2021-12-22 02:35:27 +0900

    Add Stone class

--- core.py
+++ core.py
@@ -16,3 +16,28 @@ class Coord:
 
     def get(self):
         return (self.__x, self.__y)
+
+
+class Stone:
+    # Constants to specify cell states.
+    White = 0
+    Black = 1
+    Unset = 2
+    Surrounding = 3
+    OutOfRange = 4
+
+    @staticmethod
+    def to_string(stone):
+        if stone == Stone.White:
+            return 'o'
+        elif stone == Stone.Black:
+            return 'x'
+        elif stone == Stone.Unset:
+            return '.'
+        elif stone == Stone.Surrounding:
+            return '*'
+        elif stone == Stone.OutOfRange:
+            return ' '
+        else:
+            # TODO: improve
+            raise BaseException('unreachable')
