42af7c38de5bb86c57c5c564ae85804403858c06
mityu <mityu.mail@gmail.com>
2021-12-22 16:07:33 +0900

    Add Board class

--- core.py
+++ core.py
@@ -41,3 +41,118 @@ class Stone:
         else:
             # TODO: improve
             raise BaseException('unreachable')
+
+
+class Board:
+    Size = 8
+
+    def __init__(self):
+        self.init_state()
+
+    def init_state(self):
+        # Initialize board like this:
+        #   o ... White stone
+        #   x ... Black stone
+        #   * ... Cells surrounding stones
+        #      0 1 2 3 4 5 6 7
+        #     +-+-+-+-+-+-+-+-+
+        #    0| | | | | | | | |
+        #     +-+-+-+-+-+-+-+-+
+        #    1| | | | | | | | |
+        #     +-+-+-+-+-+-+-+-+
+        #    2| | |*|*|*|*| | |
+        #     +-+-+-+-+-+-+-+-+
+        #    3| | |*|o|x|*| | |
+        #     +-+-+-+-+-+-+-+-+
+        #    4| | |*|x|o|*| | |
+        #     +-+-+-+-+-+-+-+-+
+        #    5| | |*|*|*|*| | |
+        #     +-+-+-+-+-+-+-+-+
+        #    6| | | | | | | | |
+        #     +-+-+-+-+-+-+-+-+
+        #    7| | | | | | | | |
+        #     +-+-+-+-+-+-+-+-+
+        self.__board = [[Stone.Unset for _ in range(8)] for _ in range(8)]
+        self.__stones_count = [0, 0]
+
+        self.set_stone(Coord(3, 3), Stone.White)
+        self.set_stone(Coord(4, 4), Stone.White)
+        self.set_stone(Coord(3, 4), Stone.Black)
+        self.set_stone(Coord(4, 3), Stone.Black)
+
+        surrounding_cells = [
+            Coord(2, 2),
+            Coord(2, 3),
+            Coord(2, 4),
+            Coord(2, 5),
+            Coord(3, 2),
+            Coord(3, 5),
+            Coord(4, 2),
+            Coord(4, 5),
+            Coord(5, 2),
+            Coord(5, 3),
+            Coord(5, 4),
+            Coord(5, 5),
+        ]
+        for c in surrounding_cells:
+            self.set_stone(c, Stone.Surrounding)
+
+    def is_valid_coord(self, coord):
+        (x, y) = coord.get()
+        return 0 <= x <= (Board.Size - 1) and 0 <= y <= (Board.Size - 1)
+
+    # Set stone of position 'coord' to 'stone'. Return True if success.
+    def set_stone(self, coord, stone):
+        if not self.is_valid_coord(coord):
+            return False
+        (x, y) = coord.get()
+        origin = self.get_stone(coord)
+        self.__board[x][y] = stone
+
+        # Update counts of stones
+        for color in [Stone.White, Stone.Black]:
+            if origin == color:
+                self.__stones_count[color] -= 1
+            if stone == color:
+                self.__stones_count[color] += 1
+
+        return True
+
+    def get_stone(self, coord):
+        if not self.is_valid_coord(coord):
+            return Stone.OutOfRange
+        (x, y) = coord.get()
+        return self.__board[x][y]
+
+    def get_white_stones_count(self):
+        return self.__stones_count[Stone.White]
+
+    def get_black_stones_count(self):
+        return self.__stones_count[Stone.Black]
+
+    def __str__(self):
+        visualized = ''
+        for stones in self.__board:
+            for stone in stones:
+                visualized += Stone.to_string(stone)
+            visualized += "\n"
+        return visualized[:-1]  # Remove the last "\n"
+
+    def print_board(self):
+        print("o ... White stone")
+        print("x ... Black stone")
+        print("  0 1 2 3 4 5 6 7")
+        sep = " +-+-+-+-+-+-+-+-+"
+        for linenr in range(len(self.__board)):
+            print(sep)
+            line = str(linenr)
+            for stone in self.__board[linenr]:
+                c = ' '
+                if stone == Stone.White:
+                    c = 'o'
+                elif stone == Stone.Black:
+                    c = 'x'
+                line += '|' + c
+            line += '|'
+            print(line)
+        print(sep)
--- tests/test.py
+++ tests/test.py
@@ -1,22 +1,89 @@
 import unittest as t
-from core import Coord
+import textwrap
+import core
+
+
+def trim_for_board(s):
+    return textwrap.dedent(s).strip()
+
 
 class TestCoord(t.TestCase):
     def test_init(self):
-        c = Coord(1, 1)
+        c = core.Coord(1, 1)
         self.assertEqual(c.get(), (1, 1))
 
     def test_add(self):
-        c1 = Coord(1, 2)
-        c2 = Coord(3, 4)
+        c1 = core.Coord(1, 2)
+        c2 = core.Coord(3, 4)
         self.assertEqual((c1 + c2).get(), (4, 6))
 
     def test_sub(self):
-        c1 = Coord(1, 2)
-        c2 = Coord(3, 5)
+        c1 = core.Coord(1, 2)
+        c2 = core.Coord(3, 5)
         self.assertEqual((c2 - c1).get(), (2, 3))
         self.assertEqual((c2 - c2).get(), (0, 0))
 
     def test_str(self):
-        c = Coord(1, 2)
+        c = core.Coord(1, 2)
         self.assertEqual(str(c), '(1, 2)')
+
+
+class TestBoard(t.TestCase):
+    def test_str(self):
+        c = core.Board()
+        expected = trim_for_board('''
+            ........
+            ........
+            ..****..
+            ..*ox*..
+            ..*xo*..
+            ..****..
+            ........
+            ........
+        ''')
+        self.assertEqual(str(c), expected)
+
+    def test_is_valid_coord(self):
+        c = core.Board()
+        self.assertTrue(c.is_valid_coord(core.Coord(1, 1)))
+        self.assertTrue(c.is_valid_coord(core.Coord(7, 1)))
+        self.assertTrue(c.is_valid_coord(core.Coord(1, 7)))
+        self.assertFalse(c.is_valid_coord(core.Coord(-1, 1)))
+        self.assertFalse(c.is_valid_coord(core.Coord(8, 1)))
+        self.assertFalse(c.is_valid_coord(core.Coord(1, -1)))
+        self.assertFalse(c.is_valid_coord(core.Coord(1, 8)))
+
+    def test_get_stone(self):
+        c = core.Board()
+        self.assertEqual(c.get_stone(core.Coord(0, 0)), core.Stone.Unset)
+        self.assertEqual(c.get_stone(core.Coord(2, 2)), core.Stone.Surrounding)
+        self.assertEqual(c.get_stone(core.Coord(3, 3)), core.Stone.White)
+        self.assertEqual(c.get_stone(core.Coord(3, 4)), core.Stone.Black)
+        self.assertEqual(c.get_stone(core.Coord(-1, 0)), core.Stone.OutOfRange)
+        self.assertEqual(c.get_stone(core.Coord(0, -1)), core.Stone.OutOfRange)
+        self.assertEqual(c.get_stone(core.Coord(8, 0)), core.Stone.OutOfRange)
+        self.assertEqual(c.get_stone(core.Coord(0, 8)), core.Stone.OutOfRange)
+
+    def test_set_stone(self):
+        c = core.Board()
+        p = core.Coord(0, 0)
+        c.set_stone(p, core.Stone.White)
+        self.assertEqual(c.get_stone(p), core.Stone.White)
+
+        c1, c2 = core.Board(), core.Board()
+        r = c1.set_stone(core.Coord(-1, -1), core.Stone.White)
+        self.assertFalse(r)
+        self.assertEqual(str(c1), str(c2))
+
+    def test_stones_count(self):
+        c = core.Board()
+        self.assertEqual(c.get_white_stones_count(), 2)
+        self.assertEqual(c.get_black_stones_count(), 2)
+
+        c.set_stone(core.Coord(0, 0), core.Stone.White)
+        self.assertEqual(c.get_white_stones_count(), 3)
+        self.assertEqual(c.get_black_stones_count(), 2)
+
+        c.set_stone(core.Coord(0, 0), core.Stone.Black)
+        self.assertEqual(c.get_white_stones_count(), 2)
+        self.assertEqual(c.get_black_stones_count(), 3)
