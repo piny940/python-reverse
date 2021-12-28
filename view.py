1a549917cb26ce71f46084f2dfd9b086e567c2b7
mikan <83708535+Ansai940@users.noreply.github.com>
2021-12-28 23:40:10 +0900

    Add CanvasCoord

--- view.py
+++ view.py
@@ -1,6 +1,52 @@
 import tkinter as tk
 
 
+class CanvasCoord:
+    '''
+    CanvasCoord class is 2 dimention vector to express positions in the canvas.
+    '''
+
+    def __init__(self, x, y):
+        self.__x, self.__y = x, y
+
+    def __add__(self, other):
+        return CanvasCoord(self.__x + other.__x, self.__y + other.__y)
+
+    def __sub__(self, other):
+        return CanvasCoord(self.__x - other.__x, self.__y - other.__y)
+
+    def __eq__(self, other):
+        return self.__x == other.__x and self.__y == other.__y
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __str__(self):
+        return f'({self.__x}, {self.__y})'
+
+    def set(self, newCoord):
+        self.__x, self.__y = newCoord.__x, newCoord.__y
+
+    def get(self):
+        return (self.__x, self.__y)
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
+
+
 class View:
     def create_window(self):
         window = tk.Tk()
