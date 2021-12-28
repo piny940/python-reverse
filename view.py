92c102d8a07f28018ea00391c4bd20a9120dd14a
mikan <83708535+Ansai940@users.noreply.github.com>
2021-12-28 19:36:25 +0900

    Add create_window

---
+++ view.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+
+
+class View:
+    def create_window(self):
+        window = tk.Tk()
+
+        window.title("Revrese")
+        window.geometry("500x500")
+
+        canvas = tk.Canvas(window, width=500, height=500)
+        canvas.grid(row=0, column=0)
+
+        CellSize = 50
+        BoardMargin = 50
+    
+        canvas.create_line(250, 50, 250, 450, width=400, fill="green")
+        for i in range(9):
+            canvas.create_line(
+                BoardMargin + i * CellSize, 
+                BoardMargin,
+                BoardMargin + i * CellSize, 
+                CellSize * 8 + BoardMargin, 
+                fill = "black")
+            
+            canvas.create_line(
+                BoardMargin, 
+                BoardMargin + i * CellSize,
+                CellSize * 8 + BoardMargin, 
+                BoardMargin + i * CellSize, 
+                fill = "black")
+    
+        window.mainloop()
