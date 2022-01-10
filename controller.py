63b587bdcb1eeb99e27ff49e0745bdac890afd29
mikan <83708535+Ansai940@users.noreply.github.com>
2022-01-10 18:27:15 +0900

    Add highlight

--- controller.py
+++ controller.py
@@ -58,17 +58,19 @@ class Controller(ControllerBase):
         self.__reversi.init_state()
         board = self.__reversi.get_board()
         self.__view.set_board(board)
+        self.__view.update_highlight()
 
     def request_try_put_stone(self, coord):
         self.__reversi.put_stone(coord)
+        self.__view.update_highlight()
 
     def request_switch_turn(self):
         # TODO: Switch to next turn
         pass
 
     def request_puttable_cells_for_current_player(self):
-        # TODO: Return the coords of the cells that player can put stone.
-        pass
+        color = self.__reversi.get_player_color()
+        return self.__reversi.get_puttable_coords(color)
 
     # ----- Functions to be called in core.py -----
     def request_notify_put_fails(self, coord):
@@ -81,7 +83,9 @@ class Controller(ControllerBase):
         self.__view.reverse_stones(coords)
 
     def request_notify_need_pass(self):
-        self.__view.notify_need_pass()
+        color = self.__reversi.get_player_color()
+        color = Stone.get_rival_stone_color(color)
+        self.__view.notify_need_pass(color)
 
     def request_notify_player_wins(self, color):
         self.__view.notify_player_wins(color)
